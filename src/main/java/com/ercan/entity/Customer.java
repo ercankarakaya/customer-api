523ed337354442a87e64fa3f59a54ad8c7b8e922
ercankarakaya <karakayaercan71@gmail.com>
2024-03-17 00:55:05 +0300

    customer entity created

---
+++ src/main/java/com/ercan/entity/Customer.java
@@ -0,0 +1,24 @@
+package com.ercan.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "customers")
+@Getter
+@Setter
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false)
+    private Long customerNumber;
+}
